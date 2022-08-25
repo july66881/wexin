2871b6d946cce1415ef973e89e299a87bbd92f69
ss.dog <92068148+july66881@users.noreply.github.com>
2022-08-25 22:20:17 +0800

    Update weixinpush.py

--- weixinpush.py
+++ weixinpush.py
@@ -94,8 +94,8 @@ def send_message_ceshiVX(appid, secret, template_id, weat, province, city, useri
                         'tem1': {'value': weat[2] + '°C' + ' ~ ' + weat[1] + '°C', 'color': get_color()},
                         'win': {'value': weat[3]},
                         'win1': {'value': weat[4],'color': get_color()},
-                        'verse': {'value': f'{verse()[0]}\n'
-                                           f'{" "*35}--{verse()[1]}《{verse()[2]}》','color': get_color()},
+                        'verse': {'value': f'{verse()[0]}'
+                                           f'   --{verse()[1]}','color': get_color()},
                         'one': {'value': '一言 :  ' + one[0], 'color': get_color()}
                         }
                }
