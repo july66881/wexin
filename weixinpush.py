0a099f68ac95f070c5dc6ae0fa3365a7fa64ba8c
ss.dog <92068148+july66881@users.noreply.github.com>
2022-10-03 16:27:37 +0800

    Update weixinpush.py

--- weixinpush.py
+++ weixinpush.py
@@ -127,7 +127,7 @@ def send_message_ceshiVX(appid, secret, template_id, weat, province, city, useri
                "topcolor": get_color(),
                'data': {'caihongpi': {'value': caihongpi(), 'color': get_color()},
                         'date': {'value': t + ' ' + d + '  ' + jiejari(times), 'color': get_color()},
-                        'days': {'value': f'♥今天是在一起的第{Days}天♥'+'\n'+'答应我！不要冷落我好吗？离不开你！'+'\n'+f'♥{random.choice(a)}♥' + jn, 'color': get_color()},
+                        'days': {'value': f'♥今天是在一起的第{Days}天♥'+'\n'+f'♥{random.choice(a)}♥' + jn, 'color': get_color()},
                         'daojishi': {'value': '♥'+daojishi+'♥', 'color': get_color()},
                         'citywea': {
                             'value': province + ' ' + city + '  ' + weat[0] + '  ' + weat[2] + '°C~' + weat[1] + '°C' +
